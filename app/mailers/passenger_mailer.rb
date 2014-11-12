4d616ba844eb3eae072ecc483b7a4b8ef2afd6ec
mousterian <rivladi@gmail.com>
2014-11-11 16:13:35 -0800

    added heroku url to mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -4,7 +4,7 @@ class PassengerMailer < ActionMailer::Base
     def thank_you_email(passenger)
         @passenger = passenger
         @booking = @passenger.bookings.last
-        @url = ''
+        @url = 'https://one-way-ticket.herokuapp.com'
         email_with_name = "#{@passenger.name} <#{@passenger.email}>"
         mail(to: email_with_name, subject: 'Thank you for booking at our site!')
     end
